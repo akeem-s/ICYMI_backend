ac07ae016b668372682fd6d6fc8ca31b0ba1cbf8
John Maguire <email": null>
2016-04-25 18:13:13 -0400

    Debug

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -40,7 +40,8 @@ class PlacesController < ApplicationController
     p r
     long = r["latlong"]["coords"]["longitude"]
     lat = r["latlong"]["coords"]["latitude"]
-    response = HTTParty.get("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=#{lat},#{long}&radius=30&key=" + ENV["GOOGLE_PLACE_API"])
+    response = HTTParty.get("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=#{lat},#{long}&radius=3000&key=" + ENV["GOOGLE_PLACE_API"])
+    p response
     response['results'].each do |place|
       place_id = place['place_id']
       @places << HTTParty.get('https://maps.googleapis.com/maps/api/place/details/json?placeid='+ place_id +'&key=' + ENV["GOOGLE_PLACE_API"])
